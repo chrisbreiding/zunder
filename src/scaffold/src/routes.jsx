56c055c59f32536344c1ffc9650952725f708f95
Chris Breiding <chrisbreiding@gmail.com>
2015-12-05 13:16:30 -0500

    simplify routes scaffold

--- src/scaffold/src/routes.jsx
+++ src/scaffold/src/routes.jsx
@@ -1,15 +1,10 @@
-import React, { createClass } from 'react';
-import { Router, Route, Redirect } from 'react-router';
+import React from 'react';
+import { Router, Route } from 'react-router';
 
 import App from './app/app';
 
-const Root = createClass({ render () { return this.props.children; } });
-
 export default (
   <Router>
-    <Redirect from="/" to="/app" />
-    <Route path="/" component={Root}>
-      <Route path="/app" component={App} />
-    </Route>
+    <Route path="/" component={App} />
   </Router>
 );
