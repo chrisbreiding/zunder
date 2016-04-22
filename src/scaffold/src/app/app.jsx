ec50f70d15b70c96f70f45cb633fc1dca09ae27e
Chris Breiding <chrisbreiding@gmail.com>
2016-04-22 09:04:49 -0400

    add a little flair to hello world

--- src/scaffold/src/app/app.jsx
+++ src/scaffold/src/app/app.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
 
-export default () => <h1>'Hello World!'</h1>;
+export default () => (
+  <main style={{ padding: '20px', fontSize: '20px', textAlign: 'center' }}>
+    <h1>Hello World!</h1>
+    <br />
+    <i className='fa fa-smile-o'></i>{' '}
+    <i className='fa fa-heart-o'></i>{' '}
+    <i className='fa fa-beer'></i>
+  </main>
+);
