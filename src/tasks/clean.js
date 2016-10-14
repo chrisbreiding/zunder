30f5279666f843f00ea0c621bf6056d926c16c94
Chris Breiding <chrisbreiding@gmail.com>
2016-10-14 11:02:18 -0400

    log clean tasks

--- src/tasks/clean.js
+++ src/tasks/clean.js
@@ -2,10 +2,22 @@ const del = require('del');
 
 const config = require('../lib/config');
 const { emit } = require('../lib/events')
+const util = require('../lib/util')
 
-const cleanDev = () => del(config.devDir)
-const cleanProd = () => del(config.prodDir)
-const cleanTests = () => del(config.testDir)
+const cleanDev = () => {
+  util.logSubTask('cleaning dev directory')
+  return del(config.devDir)
+}
+
+const cleanProd = () => {
+  util.logSubTask('cleaning prod directory')
+  return del(config.prodDir)
+}
+
+const cleanTests = () => {
+  util.logSubTask('cleaning test directory')
+  return del(config.testDir)
+}
 
 module.exports = (taker) => {
   return {
