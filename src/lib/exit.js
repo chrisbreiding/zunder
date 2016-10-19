1bbe58ef3c19cbe131de8aa83f2ed5fb9d639803
Chris Breiding <chrisbreiding@gmail.com>
2016-10-19 15:37:38 -0400

    fix error handling
    - exit with error stack and proper error code
    - only bind to process events when watching
    - only call exit handler once

--- src/lib/exit.js
+++ src/lib/exit.js
@@ -2,23 +2,36 @@ const util = require('./util')
 
 const closeables = []
 
-const handler = (shouldExit, err) => {
+let exited = false
+function handler (shouldExit, shouldErr = false, err) {
+  if (exited) return
+
+  exited = true
+
   closeables.forEach((closeable) => {
     if (typeof closeable.close === 'function') {
       closeable.close();
     }
   });
 
-  if (err) util.logError(err)
-  if (shouldExit) process.exit()
+  if (err && err.stack) util.logError(err.stack)
+  if (shouldExit) process.exit(shouldErr ? 1 : 0)
 }
 
-process.once('exit', handler)
-process.on('SIGINT', handler.bind(null, true))
-process.on('uncaughtException', handler.bind(null, true));
+let eventsBound = false
+function bindEvents () {
+  if (eventsBound) return
+
+  eventsBound = true
+
+  process.once('exit', handler.bind(null, false, false))
+  process.on('SIGINT', handler.bind(null, true, false))
+  process.on('uncaughtException', handler.bind(null, true, true));
+}
 
 module.exports = {
   closeOnExit (closeable) {
+    bindEvents()
     closeables.push(closeable);
   },
 }
